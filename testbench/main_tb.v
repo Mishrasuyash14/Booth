8da49690faab29a3eb440d59fd08eb42a86e60a4
Mishrasuyash14 <104592154+Mishrasuyash14@users.noreply.github.com>
2022-10-01 19:22:11 +0530

    Update main_tb.v

--- testbench/main_tb.v
+++ testbench/main_tb.v
@@ -1,1 +1,16 @@
+module test_div();
+reg [7:0]dividend ,divisor;
 
+wire [7:0]quotient ,remainder;
+int_div dut(dividend,divisor,quotient,remainder);
+initial
+begin
+ dividend = 8'b0000_1010 ; divisor = 8'b0101_0000;
+#10 dividend = 8'b01111000 ; divisor = 8'b00100010;
+#10 dividend = 8'b00011000 ; divisor = 8'b00000110;
+#10 dividend = -8'd8 ; divisor = 8'b00000010;
+#10 dividend = 8'd48 ; divisor = -8'd4;
+#10 dividend = -8'd45 ; divisor = -8'd5;
+#10 $stop;
+end 
+endmodule
